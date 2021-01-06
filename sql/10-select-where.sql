1ed87e23d65dc36af1aabec9c85879975c6e1c36
glenn <glennoph@gmail.com>
2021-01-06 15:16:13 -0600

    update sql

--- sql/10-select-where.sql
+++ sql/10-select-where.sql
@@ -2,6 +2,10 @@
 use employees;
 -- select dept_no from departments
 select dept_no from departments;
--- select all names with first name is Elvis
+-- select all employeses with first name is Elvis
 select * from employees
 	where first_name = 'Elvis';
+-- get all female employees whose first name is Kellie
+select * from employees
+	where gender = 'F' and first_name = 'Kellie';
+    
